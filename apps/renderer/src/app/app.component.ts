361b6188a8e253d5e6938e7f2c836474ba6b6df9
Daniel Breen <daninreallife7@gmail.com>
2021-06-25 16:19:21 -0600

    Add Electron

--- apps/renderer/src/app/app.component.ts
+++ apps/renderer/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nx-angular-electron-template-root',
+  selector: 'appname-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
